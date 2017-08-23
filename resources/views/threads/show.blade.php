c2f91fb57a3587ceda3f7a25ea8c3ec8eb70961d
Leonardo Lobato <leonardolobato@gmail.com>
2017-08-23 20:49:44 -0300

    initial commit

---
+++ resources/views/threads/show.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <a href="#">
+                            {{ $thread->owner->name }}
+                        </a> publicado:
+                        {{ $thread->title }}
+                    </div>
+                    <div class="panel-body">
+                        {{ $thread->body }}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach($thread->replies as $reply)
+                    @include('threads.reply')
+                @endforeach
+            </div>
+        </div>
+
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form action="{{ $thread->path() .  '/replies'}}" method="POST">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body"
+                                      id="body"
+                                      class="form-control"
+                                      placeholder="Have somthing to say?"
+                                      rows="5"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+                </div>
+            </div>
+        @endif
+    </div>
+@endsection
